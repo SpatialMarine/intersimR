03be6fabf0cb2256b32c258a49db7b1a21dd7a75
dmarch <damamo82@gmail.com>
2025-12-08 02:36:46 +0100

    add hello function, reduced logo

---
+++ R/intersimR-package.R
@@ -0,0 +1,9 @@
+# R/intersimR-package.R
+
+#' intersimR: Simulation-based detection of animal–vessel interaction events
+#'
+#' Tools to detect and quantify local interaction events from animal and vessel
+#' tracking data using simulation-based null models (attraction, following, etc.).
+#'
+#' @keywords internal
+"_PACKAGE"
